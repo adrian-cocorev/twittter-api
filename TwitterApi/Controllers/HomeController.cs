bc6b7baebe90aea83e3a1fe5ecfc6902a3831567
Adrian Cocorev <adrian.cocorev@gmail.com>
2017-04-25 18:27:19 +0300

    First commit.

---
+++ TwitterApi/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tweetinvi;
+using Tweetinvi.Models;
+using TwitterApi.Models;
+
+namespace TwitterApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpPost]
+        public ActionResult Index(string searchTerm, int tweetsNumber)
+        {
+            var tweets = GetTweets(searchTerm, tweetsNumber);
+            return View(tweets);
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var tweets = GetTweets("", 0);
+            return View(tweets);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+        private List<TweetInfo> GetTweets(string searchTerm, int tweetsNumber)
+        {
+            if(String.IsNullOrEmpty(searchTerm))
+            {
+                return new List<TweetInfo>();
+            }
+
+            Auth.SetUserCredentials("YZYc1Jyp31tKZ5zgJNWIVhLeT", "x8QRxQxtUa6JmatvMJhViVYyihm3EYpkfO89LE1jd1wAL4BGVp", "852507449630261248-hEPZwkIzKALj2sEzmumHozsl5GpcQPv", "7SOZWdC7qy7Y8zPIOj1HZ7r6CTUzB2dPUGaV89VejZiVE");
+
+            var searchParameter = Tweetinvi.Search.CreateTweetSearchParameter(searchTerm);
+            searchParameter.Lang = LanguageFilter.English;
+            searchParameter.MaximumNumberOfResults = tweetsNumber;
+            searchParameter.Since = new DateTime(2017, 1, 1);
+            var tweets = Tweetinvi.Search.SearchTweets(searchParameter);
+            return GetTweetDetails(tweets);
+        }
+
+        private List<TweetInfo> GetTweetDetails(IEnumerable<ITweet> rawTweets)
+        {
+            return rawTweets.Select(tweet => new TweetInfo()
+            {
+                UserID = tweet.CreatedBy.Id,
+                UserName = tweet.CreatedBy.Name,
+                TweetID = tweet.Id,
+                Text = tweet.Text,
+                Created = tweet.CreatedAt
+            }).ToList();
+        }
+    }
+}
---
+++ TwitterApi/Controllers/TweetsController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Tweetinvi;
+using Tweetinvi.Models;
+using Tweetinvi.Parameters;
+using Newtonsoft.Json;
+
+namespace TwitterApi.Controllers
+{
+    public class TweetDetails
+    {
+        public long UserID { get; set; }
+        public string UserName { get; set; }
+        public long TweetID { get; set; }
+        public string Text { get; set; }
+        public DateTime Created { get; set; }
+    }
+
+    public class TweetsController : ApiController
+    {
+        //public IEnumerable<TweetDetails> GetAllTweets()
+        //{
+        //    return GetTweets("google");
+        //}
+        // GET api/tweets?searchTerm="mimo"
+        public IHttpActionResult GetFilterTweets(string searchTerm)
+        {
+            var tweets = GetTweets(searchTerm);
+            if (tweets == null)
+            {
+                return NotFound();
+            }
+            return Ok(tweets);
+        }
+        
+        private IEnumerable<TweetDetails> GetTweets(string searchTerm)
+        {
+            Auth.SetUserCredentials("YZYc1Jyp31tKZ5zgJNWIVhLeT", "x8QRxQxtUa6JmatvMJhViVYyihm3EYpkfO89LE1jd1wAL4BGVp", "852507449630261248-hEPZwkIzKALj2sEzmumHozsl5GpcQPv", "7SOZWdC7qy7Y8zPIOj1HZ7r6CTUzB2dPUGaV89VejZiVE");
+
+            var searchParameter = Search.CreateTweetSearchParameter(searchTerm);
+            searchParameter.Lang = LanguageFilter.English;
+            searchParameter.MaximumNumberOfResults = 1000;
+            searchParameter.Since = new DateTime(2017, 1, 1);
+            var tweets = Search.SearchTweets(searchParameter);
+            return GetTweetDetails(tweets);
+        }
+
+        [NonAction]
+        private IEnumerable<TweetDetails> GetTweetDetails(IEnumerable<ITweet> rawTweets)
+        {
+            return rawTweets.Select(tweet => new TweetDetails()
+            {
+                UserID = tweet.CreatedBy.Id,
+                UserName = tweet.CreatedBy.Name,
+                TweetID = tweet.Id,
+                Text = tweet.Text,
+                Created = tweet.CreatedAt
+            });
+        }
+        
+        [NonAction]
+        private static List<ITweet> GetUserTimelineTweets(string userName, int maxNumberOfTweets)
+        {
+            RateLimit.RateLimitTrackerMode = RateLimitTrackerMode.TrackAndAwait;
+
+            RateLimit.QueryAwaitingForRateLimit += (sender, args) =>
+            {
+                Console.WriteLine($"Query : {args.Query} is awaiting for rate limits!");
+            };
+
+            Auth.SetUserCredentials("YZYc1Jyp31tKZ5zgJNWIVhLeT", "x8QRxQxtUa6JmatvMJhViVYyihm3EYpkfO89LE1jd1wAL4BGVp", "852507449630261248-hEPZwkIzKALj2sEzmumHozsl5GpcQPv", "7SOZWdC7qy7Y8zPIOj1HZ7r6CTUzB2dPUGaV89VejZiVE");
+            var user = Tweetinvi.User.GetUserFromScreenName(userName);
+
+            var lastTweets = Timeline.GetUserTimeline(user.Id, 200).ToArray();
+
+            var allTweets = new List<ITweet>(lastTweets);
+            var beforeLast = allTweets;
+
+            while (lastTweets.Length > 0 && allTweets.Count <= 3200)
+            {
+                var idOfOldestTweet = lastTweets.Select(x => x.Id).Min();
+                Console.WriteLine($"Oldest Tweet Id = {idOfOldestTweet}");
+
+                var numberOfTweetsToRetrieve = allTweets.Count > 3000 ? 3200 - allTweets.Count : 200;
+                var timelineRequestParameters = new UserTimelineParameters
+                {
+                    // MaxId ensures that we only get tweets that have been posted 
+                    // BEFORE the oldest tweet we received
+                    MaxId = idOfOldestTweet - 1,
+                    MaximumNumberOfTweetsToRetrieve = numberOfTweetsToRetrieve
+                };
+
+                lastTweets = Timeline.GetUserTimeline(user.Id, timelineRequestParameters).ToArray();
+                allTweets.AddRange(lastTweets);
+            }
+
+            return allTweets;
+        }
+    }
+}
